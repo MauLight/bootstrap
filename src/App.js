cdcf1f9aa8421aed8fcd1e6d708212765e7359de
MauLight <maulisseluz@gmail.com>
2023-01-31 21:16:52 -0300

    first bootstrap exercise, done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const style = {
+  "border": "solid 1px black"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-6 col-md-3" style={style}>One</div>
+        <div className="col-sm-6 col-md-3" style={style}>Two</div>
+        <div className="col-sm-6 col-md-3" style={style}>Three</div>
+        <div className="col-sm-6 col-md-3" style={style}>Four</div>
+      </div>
+
     </div>
   );
 }
