07f2441f962b9525a3671e92288d9aa9a4c15839
MauLight <maulisseluz@gmail.com>
2023-01-31 21:53:25 -0300

    toggle nav added

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,14 +2,28 @@ import React from "react";
 
 const Nav = () => {
     return (
-            <nav className="navbar bg-light px-3">
-                <a href="#" className="navbar-brand">My Website</a>
+        <nav className="navbar bg-light px-3 navbar-expand-lg">
+            <a href="#" className="navbar-brand">My Website</a>
+            <button className="navbar-toggler" data-bs-toggle= "collapse" data-bs-target= "#navCollapse">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navCollapse">
                 <ul className="navbar-nav">
                     <li className="navbar-item">
-                        <a href="#" className="nav-link">Home page</a>
+                        <a href="#" className="nav-link">Home</a>
+                    </li>
+                    <li className="navbar-item">
+                        <a href="#" className="nav-link">Love</a>
+                    </li>
+                    <li className="navbar-item">
+                        <a href="#" className="nav-link">Elv</a>
+                    </li>
+                    <li className="navbar-item">
+                        <a href="#" className="nav-link">Wife</a>
                     </li>
                 </ul>
-            </nav>
+            </div>
+        </nav>
     )
 }
 
